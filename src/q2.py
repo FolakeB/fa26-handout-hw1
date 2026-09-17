2be1660f33e9cb465d8925d6df5711c75ede0faa
FolakeB <biaou.f@northeastern.edu>
2026-09-16 23:25:01 -0400

    q2done

--- src/q2.py
+++ src/q2.py
@@ -24,4 +24,22 @@ def validate_password(password: str) -> bool:
     bool
         True if the password is valid, and false otherwise
     """
-    pass
+    has_upper = False
+    has_lower = False
+    has_digit = False
+    has_special = False
+
+    for character in password:
+        if character.isupper():
+            has_upper = True
+        elif character.islower():
+            has_lower = True
+        elif character.isdigit():
+            has_digit = True
+        elif character in "!@#$%^&*":
+            has_special = True
+
+    if len(password) >= 8 and has_upper and has_lower and has_digit and has_special:
+        return True
+    else:
+        return False
